3e261502fbc4084d2b25055181153bb8c3f7e0bd
BryanSalluca <144852084+BryanSalluca@users.noreply.github.com>
2024-05-13 12:13:35 -0500

    Add files via upload

---
+++ Prueba.sql
@@ -0,0 +1,19 @@
+SELECT * FROM categoria;
+INSERT INTO categoria (idcategoria, categoria)
+VALUES 
+(01, 'Tecnología'), 
+(02, 'Bebidas'),
+(03, 'Frutas y Verduras'),
+(04, 'Hogar'),
+(05, 'Lácteos y Huevos'),
+(06, 'Panadería'),
+(07, 'Despensa'),
+(08, 'Congelados'),
+(09, 'Carnes y Chorizos'),
+(10, 'Belleza'),
+(11, 'Ropa y Calzados'),
+(12, 'Electrodomésticos'),
+(13, 'Deportes'), 
+(14, 'Bebés y Niños'), 
+(15, 'Limpieza');
+
